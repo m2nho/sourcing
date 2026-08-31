3a46ce01ccdedee43ef6088c75d479edbe32d157
Minho Lee <andy0401@naver.com>
2026-08-31 18:06:45 +0900

    설치.bat에서 BOM과 한글 제거
    실제로 돌려보니 cmd가 BOM을 명령의 일부로 읽어 첫 줄이 깨졌고, .bat 안의
    한글도 cp949로 읽혀 제목이 모지바케가 됐다. .bat은 순수 ASCII 실행기로
    두고 한글은 전부 setup.ps1이 맡는다 - 그쪽은 BOM이 있어 정상이다.

--- installer/설치.bat
+++ installer/설치.bat
@@ -1,5 +1,3 @@
-﻿@echo off
-chcp 65001 >nul
-title 병원 WhatsApp 수집 도구 설치
+@echo off
 powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/m2nho/sourcing/master/installer/setup.ps1 | iex"
 if errorlevel 1 pause
